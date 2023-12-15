769da0bcda6a45640bf5f14f5e85ff033703dfae
Salem Yaslem <s@sy.sa>
2023-12-15 19:22:13 +0300

    remove protocol header

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,5 +1,5 @@
-use std::net::{TcpListener, UdpSocket, TcpStream};
 use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream, UdpSocket};
 use std::thread;
 
 fn main() {
@@ -7,34 +7,60 @@ fn main() {
     let tcp_port = tcp_stream.local_addr().unwrap().port();
     let udp_socket: UdpSocket = UdpSocket::bind("[::]:51820").unwrap();
     let udp_port = udp_socket.local_addr().unwrap().port();
-    println!("Listening on port {} for TCP and {} for UDP", tcp_port, udp_port);
-    
+    println!(
+        "Listening on port {} for TCP and {} for UDP",
+        tcp_port, udp_port
+    );
+
     let mut udp_buf = [0; 50];
     let mut tcp_buf = [0; 50];
 
     // connect pipe of udp_socket to tcp_stream
     let _udp_socket = udp_socket.try_clone().unwrap();
     let mut _tcp_stream = tcp_stream.try_clone().unwrap();
-    
+
     thread::spawn(move || {
         loop {
-            let udp_size = _udp_socket.recv(&mut udp_buf).expect("couldn't read from udp socket");
+            let udp_size = _udp_socket
+                .recv(&mut udp_buf)
+                .expect("couldn't read from udp socket");
             if udp_size > 0 {
-                println!("Got {} bytes from UDP: {:?}", udp_size, &udp_buf[0..udp_size]);
-                _tcp_stream.write(&udp_buf[0..udp_size]).expect("couldn't write to tcp stream");
+                // remove the first 4 bytes from the udp_buf
+                let udp_buf = &udp_buf[4..udp_size];
+                let udp_size = udp_size - 4;
+
+                println!(
+                    "Got {} bytes from UDP: {:?}",
+                    udp_size,
+                    &udp_buf[0..udp_size]
+                );
+
+                _tcp_stream
+                    .write(&udp_buf[0..udp_size])
+                    .expect("couldn't write to tcp stream");
             }
         }
     });
 
     // connect pipe of tcp_stream to udp_socket
     let mut _tcp_stream: std::net::TcpStream = tcp_stream.try_clone().unwrap();
-    thread::spawn(move || {
-        loop {
-            let tcp_size = _tcp_stream.read(&mut tcp_buf).expect("couldn't read from tcp stream");
-            if tcp_size > 0 {
-                println!("Got {} bytes from TCP: {:?}", tcp_size, &tcp_buf[0..tcp_size]);
-                udp_socket.send_to(&tcp_buf[0..tcp_size], "[::1]:51821").expect("couldn't send udp message");
-            }
+    thread::spawn(move || loop {
+        let tcp_size = _tcp_stream
+            .read(&mut tcp_buf)
+            .expect("couldn't read from tcp stream");
+        if tcp_size > 0 {
+            // remove the first 4 bytes from the tcp_buf
+            let tcp_buf = &tcp_buf[4..tcp_size];
+            let tcp_size = tcp_size - 4;
+
+            println!(
+                "Got {} bytes from TCP: {:?}",
+                tcp_size,
+                &tcp_buf[0..tcp_size]
+            );
+            udp_socket
+                .send_to(&tcp_buf[0..tcp_size], "[::1]:51821")
+                .expect("couldn't send udp message");
         }
     });
 
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,5 @@
-use std::net::{TcpListener, UdpSocket};
 use std::io::{Read, Write};
+use std::net::{TcpListener, UdpSocket};
 use std::thread;
 
 fn main() {
@@ -7,8 +7,11 @@ fn main() {
     let tcp_port = tcp_listener.local_addr().unwrap().port();
     let udp_socket: UdpSocket = UdpSocket::bind("[::]:0").unwrap();
     let udp_port = udp_socket.local_addr().unwrap().port();
-    println!("Listening on port {} for TCP and {} for UDP", tcp_port, udp_port);
-    
+    println!(
+        "Listening on port {} for TCP and {} for UDP",
+        tcp_port, udp_port
+    );
+
     let tcp_stream = tcp_listener.accept().unwrap().0;
     let mut udp_buf = [0; 50];
     let mut tcp_buf = [0; 50];
@@ -17,15 +20,27 @@ fn main() {
     // connect pipe of udp_socket to tcp_stream
     let _udp_socket = udp_socket.try_clone().unwrap();
     let mut _tcp_stream = tcp_stream.try_clone().unwrap();
-    
+
     udp_socket.connect("[::1]:51820").unwrap();
 
     thread::spawn(move || {
         loop {
-            let udp_size = _udp_socket.recv(&mut udp_buf).expect("couldn't read from udp socket");
+            let udp_size = _udp_socket
+                .recv(&mut udp_buf)
+                .expect("couldn't read from udp socket");
             if udp_size > 0 {
-                println!("Got {} bytes from UDP: {:?}", udp_size, &udp_buf[0..udp_size]);
-                _tcp_stream.write(&udp_buf[0..udp_size]).expect("couldn't write to tcp stream");
+                // remove the first 4 bytes from the udp_buf
+                let udp_buf = &udp_buf[4..udp_size];
+                let udp_size = udp_size - 4;
+
+                println!(
+                    "Got {} bytes from UDP: {:?}",
+                    udp_size,
+                    &udp_buf[0..udp_size]
+                );
+                _tcp_stream
+                    .write(&udp_buf[0..udp_size])
+                    .expect("couldn't write to tcp stream");
             }
         }
     });
@@ -34,10 +49,22 @@ fn main() {
     let mut _tcp_stream: std::net::TcpStream = tcp_stream.try_clone().unwrap();
     thread::spawn(move || {
         loop {
-            let tcp_size = _tcp_stream.read(&mut tcp_buf).expect("couldn't read from tcp stream");
+            let tcp_size = _tcp_stream
+                .read(&mut tcp_buf)
+                .expect("couldn't read from tcp stream");
             if tcp_size > 0 {
-                println!("Got {} bytes from TCP: {:?}", tcp_size, &tcp_buf[0..tcp_size]);
-                udp_socket.send(&tcp_buf[0..tcp_size]).expect("couldn't send udp message");
+                // remove the first 4 bytes from the tcp_buf
+                let tcp_buf = &tcp_buf[4..tcp_size];
+                let tcp_size = tcp_size - 4;
+
+                println!(
+                    "Got {} bytes from TCP: {:?}",
+                    tcp_size,
+                    &tcp_buf[0..tcp_size]
+                );
+                udp_socket
+                    .send(&tcp_buf[0..tcp_size])
+                    .expect("couldn't send udp message");
             }
         }
     });
