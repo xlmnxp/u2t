c2d1d25155fcdd523e750a28167e000675acf74e
Salem Yaslem <s@sy.sa>
2023-12-15 18:55:57 +0300

    use threads for pipe

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,29 +1,42 @@
 use std::net::{TcpListener, UdpSocket};
 use std::io::{Read, Write};
+use std::thread;
 
 fn main() {
     let tcp_listener = TcpListener::bind("[::]:1422").unwrap();
     let tcp_port = tcp_listener.local_addr().unwrap().port();
-    let udp_socket = UdpSocket::bind("[::]:0").unwrap();
+    let udp_socket: UdpSocket = UdpSocket::bind("[::]:0").unwrap();
     let udp_port = udp_socket.local_addr().unwrap().port();
     println!("Listening on port {} for TCP and {} for UDP", tcp_port, udp_port);
     
-    let mut tcp_stream = tcp_listener.accept().unwrap().0;
+    let tcp_stream = tcp_listener.accept().unwrap().0;
     let mut udp_buf = [0; 50];
 
-    println!("Got a connection!");
+    // connect pipe of udp_socket to tcp_stream
+    let _udp_socket = udp_socket.try_clone().unwrap();
+    let mut _tcp_stream = tcp_stream.try_clone().unwrap();
     
-    loop {
-        let mut tcp_buf = [0; 50];
-        let tcp_size = tcp_stream.read(&mut tcp_buf).unwrap();
-        if tcp_size > 0 {
-            println!("Got {} bytes from TCP", tcp_size);
-            udp_socket.send_to(&tcp_buf[0..tcp_size], "[::]:51820").unwrap();
+    thread::spawn(move || {
+        loop {
+            let udp_size = _udp_socket.recv(&mut udp_buf).unwrap();
+            if udp_size > 0 {
+                println!("Got {} bytes from UDP", udp_size);
+                _tcp_stream.write(&udp_buf[0..udp_size]).unwrap();
+            }
         }
-        let udp_size = udp_socket.recv(&mut udp_buf).unwrap();
-        if udp_size > 0 {
-            println!("Got {} bytes from UDP", udp_size);
-            tcp_stream.write(&udp_buf[0..udp_size]).unwrap();
+    });
+
+    // connect pipe of tcp_stream to udp_socket
+    let mut _tcp_stream: std::net::TcpStream = tcp_stream.try_clone().unwrap();
+    thread::spawn(move || {
+        loop {
+            let mut tcp_buf = [0; 50];
+            let tcp_size = _tcp_stream.read(&mut tcp_buf).unwrap();
+            if tcp_size > 0 {
+                println!("Got {} bytes from TCP", tcp_size);
+                udp_socket.send_to(&tcp_buf[0..tcp_size], "[::]:51821").unwrap();
+            }
         }
-    }
+    });
+
 }
